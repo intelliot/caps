629683695f5a6428ebf769654ad7bf0a5df4c8cd
System Administrator <root@Wang-Yupings-Mac-Pro.local>
2012-05-14 10:37:13 -0400

    Initial Commit

---
+++ PhotoApp/PhotoAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PhotoAppAppDelegate.h
+//  PhotoApp
+//
+//  Created by System Administrator on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoApp
+//
+//  Created by System Administrator on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PhotoAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PhotoAppAppDelegate class]));
+    }
+}
